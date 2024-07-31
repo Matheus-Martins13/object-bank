42457152509a6f16ff6f1e004db7f7efc8ec8d5d
Matheus Martins <matheusmmartins8@gmail.com>
2024-07-30 22:07:55 -0300

    feat: implementa tratativa de busca para as categorias

--- src/components/home-components/Categories/Categories.tsx
+++ src/components/home-components/Categories/Categories.tsx
@@ -13,7 +13,7 @@ export const Categories = () => {
 
     const categories: CategoryWithObjectsDto[] =
       await findAllCategoriesWithObjects();
-      console.log(categories);
+    console.log(categories);
     setCategories(categories);
   };
 
@@ -23,11 +23,19 @@ export const Categories = () => {
 
   return (
     <div className="w-11/12 sm:w-3/5 lg:w-2/4 lg:self-start lg:ms-10">
-      {categories?.map((cat) => (
-        <div key={cat.idCategory} className="text-black">
-          <Category key={cat.idCategory} cat={cat}/>
-        </div>
-      ))}
+      {categories ? (
+        categories.length > 0 ? (
+          categories?.map((cat) => (
+            <div key={cat.idCategory}>
+              <Category key={cat.idCategory} cat={cat} />
+            </div>
+          ))
+        ) : (
+          <div className="text-black">Nenhuma categoria cadastrada</div>
+        )
+      ) : (
+        <div className='text-black'>Carregando...</div>
+      )}
     </div>
   );
 };
